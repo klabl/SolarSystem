f992250649f9d4d4cdf8246ad8a7ce902fbb2f35
Pwngu <klaus.ableitinger@gmail.com>
2015-11-30 23:27:28 +0100

    initital commit, missing .gitignore

---
+++ src/Orbs.py
@@ -0,0 +1,56 @@
+from abc import *
+
+__author__ = 'klaus'
+
+
+class Orb(ABC):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, name, radius, surface, year_scale=1, day_scale=1, rotation_cw=True):
+
+        self.__system_center = None
+        self.__system = []
+        self.__name = name
+        self.__radius = radius
+        self.__surface = surface
+        self.__year_scale = year_scale
+        self.__day_scale = day_scale
+        self.__rotation_cw = rotation_cw
+
+    def add_orb(self, orb):
+
+        if isinstance(orb, Orb):
+            self.__system.append(orb)
+        else:
+            raise Exception
+
+    def get_system(self):
+
+        return self.__system
+
+    def set_system_center(self, orb):
+
+        if isinstance(orb, Orb):
+            self.__system_center = orb
+        else:
+            raise Exception
+
+
+class Planet(Orb):
+    __metaclass__ = Orb
+
+
+class Star(Orb):
+    __metaclass__ = Orb
+
+    def __init__(self, name, radius, year_scale=1, day_scale=1, rotation_cw=True, light_strength=1):
+        super().__init__(name, radius, year_scale, day_scale, rotation_cw)
+        self.__light_strength = light_strength
+
+
+class Surface(object):
+
+    def __init__(self, size, pattern, circle):
+        self.__size = size
+        self.__pattern = pattern
+        self.__circle = circle
---
+++ src/Simulation.py
@@ -0,0 +1,25 @@
+__author__ = 'klaus'
+
+
+def init():
+    pass
+
+
+def set_speed(speed):
+    pass
+
+
+def accelerate():
+    pass
+
+
+def deccelerate():
+    pass
+
+
+def show_textures(show=True):
+    pass
+
+
+def pause(pause=True):
+    pass
