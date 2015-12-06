eeca7d4abc3adb0a132f09e113260add28370e31
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-06 23:47:38 +0100

    Finished example doc

--- src/main/Orb.py
+++ src/main/Orb.py
@@ -1,50 +1,92 @@
 from abc import *
+from math import*
+from src.tmp.Drawable import Drawable
 
+from pyglet.gl.gl import *
+from pyglet.gl.glu import *
 __author__ = 'pwngu'
 
 
 class Orb:
+    # What does metaclass mean? Super class?
     __metaclass__ = ABCMeta
 
     def __init__(self, name, radius, surface, year_scale=1, day_scale=1, rotation_cw=True):
-        self.__system_center = None
-        self.__system = []
-        self.__name = name
-        self.__radius = radius
-        self.__surface = surface
-        self.__year_scale = year_scale
-        self.__day_scale = day_scale
-        self.__rotation_cw = rotation_cw
+        self.system_center = None
+        self.system = []
+        self.name = name
+        self.radius = radius
+        self.surface = surface
+        self.year_scale = year_scale
+        self.day_scale = day_scale
+        self.rotation_cw = rotation_cw
 
     def add_orb(self, orb):
 
         if isinstance(orb, Orb):
-            self.__system.append(orb)
+            self.system.append(orb)
         else:
             raise Exception
 
     def get_system(self):
-
-        return self.__system
+        return self.system
 
     def set_system_center(self, orb):
-
         if isinstance(orb, Orb):
-            self.__system_center = orb
+            self.system_center = orb
         else:
             raise Exception
 
+    @abstractmethod
+    def update(self, time):
+        raise NotImplementedError("Abstract Method not implemented yet")
+
+    @abstractmethod
+    def draw(self):
+        raise NotImplementedError("Abstract Method not implemented yet")
+
 
 class Planet(Orb):
-    __metaclass__ = Orb
+
+    def __init__(self, name, radius, surface, year_scale=1, day_scale=1, rotation_cw_=True):
+        super(Planet, self).__init__(name, radius, surface, year_scale, day_scale)
+        self.cur_rotation_angle = 0
+        self.cur_rotation_angle_year = 0
+        self.pos_x, self.pos_y, self.pos_z = 0, 0, 0
+
+    def draw(self):
+        # TODO alle Parameter auf attribute beziehen z.B. bei rotate
+        # Reihenfolge: Erst wird transliert DANN rotiert
+            # das was ganz unten steht wird als erstes ausgefuehrt
+        # yearscale
+        glRotatef(self.cur_rotation_angle_year, 0, 1, 0)
+        glTranslatef(self.pos_x, self.pos_y, self.pos_z)
+        # dayscale
+        glRotatef(self.cur_rotation_angle, 0, 1, 0)
+        gluSphere(self.surface, self.radius, 20, 12)
+
+    def update(self, time):
+        """
+        Hier werden alle Werte berechnet
+
+        :param time: Zeit
+        :return:
+        """
+        self.cur_rotation_angle += self.day_scale
+        self.cur_rotation_angle_year += self.year_scale
 
 
 class Star(Orb):
-    __metaclass__ = Orb
 
     def __init__(self, name, radius, year_scale=1, day_scale=1, rotation_cw=True, light_strength=1):
         super(Star, self).__init__(name, radius, year_scale, day_scale, rotation_cw)
         self.__light_strength = light_strength
+
+    def update(self, time):
+        raise NotImplementedError("Abstract Method not implemented yet")
+
+    def draw(self):
+        raise NotImplementedError("Abstract Method not implemented yet")
 
 
 class Surface(object):
--- src/main/Simulation.py
+++ src/main/Simulation.py
@@ -1,8 +1,68 @@
-__author__ = 'klaus'
+import pyglet
+from pyglet.gl.glu import *
+from pyglet.gl.gl import *
+
+from src.main.Orb import Planet
+
+__author__ = 'mreilaender'
+
+# asd
+
+window = pyglet.window.Window(resizable=False, fullscreen=True)
 
 
 def init():
+    # Initialize planets
     pass
+
+
+@window.event
+def on_draw():
+    window.clear()
+
+    # 3r Projektion einstellen
+    glEnable(GL_DEPTH_TEST)
+    # Kamera auf Fenstergroesse einstellen
+    glMatrixMode(GL_PROJECTION)
+    glViewport(0, 0, window.width, window.height)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glLoadIdentity()
+
+    # Lege Perspektive fest
+    gluPerspective(45, window.width/window.height, 0.1, 100.0)
+
+    #
+    gluLookAt(
+            0, 60, 0,
+            0, 0, 0,
+            0, 0, 1
+            )
+
+    # Lade ModelView Matrix
+    glMatrixMode(GL_MODELVIEW)
+
+    glLoadIdentity()
+    glRotatef(45, 1, 0, 0)
+    planet1.draw()
+
+    glLoadIdentity()
+    # Transliert das Objekt um 10 in die positive x Richtung
+    planet2.pos_x = 10
+    glRotatef(-45, 1, 0, 0)
+    planet2.draw()
+
+
+def update(time):
+    """
+    Update Method - hier wird nur berechnet nicht gezeichnet, bzw die update Methoden der Planeten aufgerufen
+    :param time: Time which has past during the last call of this method. Pyglet calculates this automatically
+    :return:
+    """
+    # TODO evtl Liste aller Planeten -> dann muss man nurmehr mit einer for durchgehen und die update Methode aufrufen
+    # print("Time: %s" % time)
+    planet1.update(time)
+    planet2.update(time)
+    # print("x: %s y: %s z: %s" % (planet2.pos_x, planet2.pos_y, planet2.pos_z))
 
 
 def set_speed(speed):
@@ -10,6 +70,7 @@ def set_speed(speed):
 
 
 def accelerate():
+    # which parameters?
     pass
 
 
@@ -23,3 +84,13 @@ def show_textures(show=True):
 
 def pause(pause=True):
     pass
+
+
+# Initialize planets
+# gluNewQuadrtic ist die standard textur
+planet1 = Planet("Test Planet", 3, gluNewQuadric())
+planet2 = Planet("Test Planet2", 1, gluNewQuadric(), day_scale=0.5)
+
+# Setting update method
+pyglet.clock.schedule(update)
+pyglet.app.run()
